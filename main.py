53c631f0d06920259f7773ebe72684bdff3c8968
Devansh Verma <142670298+DevanshV03@users.noreply.github.com>
2025-03-28 01:36:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ class Trig(db.Model):
 
 
 class User(UserMixin,db.Model):
-    id=db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username=db.Column(db.String(50))
     email=db.Column(db.String(50),unique=True)
     password=db.Column(db.String(1000))
